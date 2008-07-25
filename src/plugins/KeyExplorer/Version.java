592bc87685cedd100338eed7c7c4f753691b767d
saces <saces@freenetproject.org>
2008-07-25 23:20:58 +0000

    fix build, add version
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/KeyExplorer@21413 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/plugins/KeyExplorer/Version.java
@@ -0,0 +1,12 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.KeyExplorer;
+
+/**
+ * @author saces
+ *
+ */
+public class Version {
+	public static final String svnRevision = "@custom@";
+}
