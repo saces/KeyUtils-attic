413104f3f8584c62150bb8aa35615a5a7a212cdb
Matthew Toseland <toad@amphibian.dyndns.org>
2009-04-09 22:14:11 +0000

    Version KeyExplorer
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/KeyExplorer@26687 67a373e5-eb02-0410-a15c-ee090a768436

--- Version.java
+++ Version.java
@@ -8,5 +8,16 @@ package plugins.KeyExplorer;
  *
  */
 public class Version {
+	/** SVN revision number. Only set if the plugin is compiled properly e.g. by emu. */
 	public static final String svnRevision = "@custom@";
+	
+	/** Version number of the plugin for getRealVersion(). Increment this on making
+	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
+	 * etc, at a minimum any build inserted into auto-update should have a unique 
+	 * version.
+	 * 
+	 * I have set this to 3000 to allow encoding marketing versions into real versions.
+	 * Long provides plenty of leeway! Toad. */
+	public static long version = 3000;
+
 }
