55ea55cb3879b15a4016c94391b82a2c4c3bf50e
saces <saces@freenetproject.org>
2009-08-30 13:21:16 +0200

    fix removed 'impossible' throw declaration

--- src/plugins/KeyExplorer/KeyExplorer.java
+++ src/plugins/KeyExplorer/KeyExplorer.java
@@ -618,12 +618,7 @@ public class KeyExplorer implements FredPlugin, FredPluginHTTP, FredPluginL10n, 
 		if (md.isSimpleManifest()) {
 			// a subdir
 			HashMap<String, Metadata> docs = md.getDocuments();
-			Metadata defaultDoc = null;
-			try {
-				defaultDoc = md.getDefaultDocument();
-			} catch (MetadataParseException e) {
-				//impossible;
-			}
+			Metadata defaultDoc = md.getDefaultDocument();
 
 			htmlTableRow.addChild(makeNameCell(prefix, name));
 			htmlTableRow.addChild(makeCell("(" + Integer.toString(docs.size())+" Items)"));
