e04f61b258408065c7da0caf539017af5424b68f
saces <saces@freenetproject.org>
2009-09-20 20:53:39 +0200

    - doc - add isFormPassword()

--- src/plugins/fproxy/lib/WebInterfaceToadlet.java
+++ src/plugins/fproxy/lib/WebInterfaceToadlet.java
@@ -3,6 +3,7 @@ package plugins.fproxy.lib;
 import freenet.clients.http.LinkEnabledCallback;
 import freenet.clients.http.Toadlet;
 import freenet.clients.http.ToadletContext;
+import freenet.support.api.HTTPRequest;
 
 public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabledCallback {
 
@@ -29,6 +30,10 @@ public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabled
 		return true;
 	}
 
+	/**
+	 * @param path
+	 * @return the path without "/path/to/toadlet", returns allways at min "/", so "/path/to/toadlet" and "/path/to/toadlet/" cant be distinguished 
+	 */
 	protected String normalizePath(String path) {
 		String result = path.substring(_path.length());
 		if (result.length() == 0) {
@@ -36,4 +41,14 @@ public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabled
 		}
 		return result;
 	}
+
+	/**
+	 * @param req
+	 * @return true if the form password is valid
+	 */
+	protected boolean isFormPassword(HTTPRequest req) {
+		String passwd = req.getPartAsString("formPassword", 32);
+		return (passwd != null) && passwd.equals(pluginContext.clientCore.formPassword);
+	}
+
 }
